93ba571ab1d4e9673b8c0a94e76dfc22bf05afdf
MmaverickK <anurag101991@gmail.com>
2023-04-08 21:10:21 +0530

    test

--- src/Test.java
+++ src/Test.java
@@ -5,28 +5,29 @@ import java.util.stream.Collectors;
 public class Test {
 
 	public static void main(String[] args) {
-		int[] a = {5,3,6,1,3};
-		int K= 2;
-		
-//		int[] a = {8,8,4,3};
+//		int[] a = {5,3,6,1,3};
 //		int K= 2;
-		
-		int min = Integer.MAX_VALUE;
-		for(int i=0; i<a.length-1; i++) {
-			int p = i;
-			int q= i+K-1;
-			for(int j=i+1; j<a.length; j++) {
-				int abs = Math.abs(a[i]-a[j]);
-//				if(Math.abs(i-j) && abs < min) {
-//					min = abs;
-//				}
-			}
-		}
-		System.out.println("min :: "+Math.abs(min));
-		List<Integer> list = Arrays.stream(a)
-			      .boxed()
-			      .collect(Collectors.toList());
-		
+//
+////		int[] a = {8,8,4,3};
+////		int K= 2;
+//
+//		int min = Integer.MAX_VALUE;
+//		for(int i=0; i<a.length-1; i++) {
+//			int p = i;
+//			int q= i+K-1;
+//			for(int j=i+1; j<a.length; j++) {
+//				int abs = Math.abs(a[i]-a[j]);
+////				if(Math.abs(i-j) && abs < min) {
+////					min = abs;
+////				}
+//			}
+		System.out.println("hello!");
+	}
+//        System.out.println("min :: "+Math.abs(min));
+//	List<Integer> list = Arrays.stream(a)
+//			.boxed()
+//			.collect(Collectors.toList());
+
 //		for(int i=0; i<list.size(); i++) {
 //			for(int j=i+1; j<list.size(); j++) {
 //				if(j > i+K)
@@ -34,11 +35,10 @@ public class Test {
 //			}
 //				
 //		}
-		
+
 //		for(In)
-		
-		
-		
-	}
-	
+
+
 }
+
+
